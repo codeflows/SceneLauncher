71f6371e15f01b261cb98101278da3cdaf3e6c76
Jari Aarniala <jari.aarniala@gmail.com>
2014-12-06 11:25:16 +0200

    Add Play/Pause button

--- LivePlaylist/ViewController.swift
+++ LivePlaylist/ViewController.swift
@@ -1,35 +1,31 @@
 import UIKit
 
-class ViewController: UIViewController, OSCServerDelegate {
+class ViewController: UIViewController {
     let label = UILabel()
+    let button = UIButton()
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         label.text = "LivePlaylist"
         label.textAlignment = NSTextAlignment.Center
-        label.backgroundColor = UIColor.redColor()
+        label.frame = CGRectMake(0, 50, 150, 50)
         view.addSubview(label)
         view.backgroundColor = UIColor.greenColor()
         
-        // TODO temporary
-        let server = OSCServer()
-        server.delegate = self
-        server.listen(8000)
-        
+        button.addTarget(self, action: "playPressed:", forControlEvents: UIControlEvents.TouchUpInside)
+        button.setTitle("Play/Pause", forState: .Normal)
+        button.frame = CGRectMake(0, 150, 150, 50)
+        button.backgroundColor = UIColor.redColor()
+        view.addSubview(button)
+    }
+    
+    func playPressed(sender: UIButton!) {
         NSLog("Boom, sending message to Reaper")
         let client = OSCClient()
         let message = OSCMessage(address: "/play", arguments: [])
         client.sendMessage(message, to: "udp://localhost:9000")
         NSLog("Done sending")
-    }
-    
-    func handleMessage(message: OSCMessage) {
-        NSLog("Received message \(message)!")
-    }
-    
-    override func viewDidLayoutSubviews() {
-        label.frame = view.bounds
     }
     
     override func didReceiveMemoryWarning() {
