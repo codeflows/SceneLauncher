9060659b1d8f7299c6b479543951770420eaa2d5
Jari Aarniala <jari.aarniala@gmail.com>
2014-12-08 10:26:30 +0200

    Make controller a CollectionViewController

--- LivePlaylist/ViewController.swift
+++ LivePlaylist/ViewController.swift
@@ -1,17 +1,28 @@
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UICollectionViewController {
     let label = UILabel()
     let button = UIButton()
+
+    override init() {
+        let layout = UICollectionViewFlowLayout()
+        super.init(collectionViewLayout: layout)
+        //collectionView!.registerClass(CollectionViewNumbersCell.self, forCellWithReuseIdentifier: CellId)
+        //collectionView!.dataSource = dataSource
+    }
+
+    required init(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        collectionView!.backgroundColor = UIColor.whiteColor()
 
         label.text = "LivePlaylist"
         label.textAlignment = NSTextAlignment.Center
         label.frame = CGRectMake(0, 50, 150, 50)
         view.addSubview(label)
-        view.backgroundColor = UIColor.greenColor()
         
         button.addTarget(self, action: "playPressed:", forControlEvents: UIControlEvents.TouchUpInside)
         button.setTitle("Play/Pause", forState: .Normal)
