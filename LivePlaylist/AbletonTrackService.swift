4cb093148bca254cecfb2bf72a8b6169fbc5211d
Jari Aarniala <jari.aarniala@gmail.com>
2014-12-16 19:34:26 +0200

    Pseudo code impl

--- LivePlaylist/AbletonTrackService.swift
+++ LivePlaylist/AbletonTrackService.swift
@@ -2,6 +2,15 @@ import ReactiveCocoa
 
 class AbletonTrackService : TrackService {
     func listTracks() -> [String] {
+        // Pseudo-implementation:
+        //
+        // To get number of scenes (maybe needed so that we know all scenes have arrived)
+        // - send /live/scenes
+        // - receive /live/scenes (int)
+        //
+        // To get names of all scenes
+        // - send /live/name/scene
+        // - receive series of /live/name/scene (int scene, string name)
         return ["Boom", "Boom", "Boom"]
     }
 }
