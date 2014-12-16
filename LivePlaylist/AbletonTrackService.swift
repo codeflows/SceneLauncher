b68da0aaf60f462f895dc227766e8634254000d0
Jari Aarniala <jari.aarniala@gmail.com>
2014-12-16 19:18:02 +0200

    Remove toy example

--- LivePlaylist/AbletonTrackService.swift
+++ LivePlaylist/AbletonTrackService.swift
@@ -1,24 +1,7 @@
 import ReactiveCocoa
 
 class AbletonTrackService : TrackService {
-    class Person: NSObject {
-        var name = "John Doe"
-        
-        override init() {
-            super.init()
-        }
-    }
-    
     func listTracks() -> [String] {
-        let person = Person()
-        let nameChanged = person.rac_valuesForKeyPath("name", observer: person)
-        
-        nameChanged.subscribeNext { (name) -> Void in
-            println("Person's new name is \(name)")
-        }
-        
-        person.setValue("Jari", forKey: "name")
-        
         return ["Boom", "Boom", "Boom"]
     }
 }
