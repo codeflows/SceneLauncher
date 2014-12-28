7e6de9fd25516273b9533c563bb66b55f355271d
Jari Aarniala <jari.aarniala@gmail.com>
2014-12-28 19:17:58 +0200

    Refactor

--- LivePlaylist/AbletonTrackService.swift
+++ LivePlaylist/AbletonTrackService.swift
@@ -4,10 +4,6 @@ class AbletonTrackService : NSObject, TrackService {
   func listTracks(callback: ([String]) -> ()) {
     // Pseudo-implementation:
     //
-    // To get number of scenes (maybe needed so that we know all scenes have arrived)
-    // - send /live/scenes
-    // - receive /live/scenes (int)
-    //
     // To get names of all scenes
     // - send /live/name/scene
     // - receive series of /live/name/scene (int scene, string name)
@@ -16,11 +12,15 @@ class AbletonTrackService : NSObject, TrackService {
     osc.sendMessage(message)
     
     // TODO make this time out after an approriate amount of time
-    osc.incomingMessagesSignal
-      .filter { $0.address == "/live/scenes" }
-      .take(1)
-      .map { ["You will receive \($0.arguments[0]) scenes"] }
-      .observe(callback)
+    let numberOfScenes =
+      osc.incomingMessagesSignal
+        .filter { $0.address == "/live/scenes" }
+        .take(1)
+        .map { $0.arguments[0] }
+
+    numberOfScenes.observe { number in
+      NSLog("This project has \(number) scenes")
+    }
   }
 }
 
