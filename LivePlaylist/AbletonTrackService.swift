40fc37b4f740c1ebb5a1bf89db057bb346f5b42b
Jari Aarniala <jari.aarniala@gmail.com>
2015-01-12 19:27:40 +0200

    Skeleton for querying scene names

--- LivePlaylist/AbletonTrackService.swift
+++ LivePlaylist/AbletonTrackService.swift
@@ -4,12 +4,6 @@ class AbletonTrackService : NSObject, TrackService {
   let osc = OSCService()
   
   func listTracks(callback: ([String]) -> ()) {
-    // Pseudo-implementation:
-    //
-    // To get names of all scenes
-    // - send /live/name/scene
-    // - receive series of /live/name/scene (int scene, string name)
-    
     let message = OSCMessage(address: "/live/scenes", arguments: [])
     osc.sendMessage(message)
     
@@ -20,6 +14,13 @@ class AbletonTrackService : NSObject, TrackService {
         .filter { $0.address == "/live/scenes" }
         .take(1)
         .map { $0.arguments[0] as Int }
+
+    let sceneNames : HotSignal<String> = numberOfScenes.mergeMap { n in
+      NSLog("Will map \(n) tracks to their names")
+      return HotSignal.never()
+    }
+    
+    sceneNames.observe { NSLog("Names are \($0)") }
     
     /*let x : HotSignal<OSCMessage> = numberOfScenes.mergeMap { n in
       NSLog("BOOM! Getting names for \(n) scenes")
