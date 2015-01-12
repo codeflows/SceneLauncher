215f6a5064df9130cc5de7d264a77af68a0dd772
Jari Aarniala <jari.aarniala@gmail.com>
2015-01-12 10:34:29 +0200

    Type for numberOfScenes signal

--- LivePlaylist/AbletonTrackService.swift
+++ LivePlaylist/AbletonTrackService.swift
@@ -1,3 +1,5 @@
+import ReactiveCocoa
+
 class AbletonTrackService : NSObject, TrackService {
   let osc = OSCService()
   
@@ -13,21 +15,19 @@ class AbletonTrackService : NSObject, TrackService {
     
     // TODO make this time out after an approriate amount of time
     // TODO LiveOsc(?) fails if Scene name contains Unicode characters and returns /remix/error
-    let numberOfScenes =
+    let numberOfScenes : HotSignal<Int> =
       osc.incomingMessagesSignal
         .filter { $0.address == "/live/scenes" }
         .take(1)
-        .map { $0.arguments[0] }
-
-    numberOfScenes.observe { number in
-      callback(["There are \(number) of scenes"])
-    }
-
-    // TODO flatMap this from numberOfScenes, take(numberOfScenes)
-    osc.sendMessage(OSCMessage(address: "/live/name/scene", arguments: []))
-    osc.incomingMessagesSignal
-      .filter { $0.address == "/live/name/scene" }
-      .observe { NSLog("Scene #\($0.arguments[0]): \($0.arguments[1])") }
+        .map { $0.arguments[0] as Int }
+    
+    /*let x : HotSignal<OSCMessage> = numberOfScenes.mergeMap { n in
+      NSLog("BOOM! Getting names for \(n) scenes")
+      osc.sendMessage(OSCMessage(address: "/live/name/scene", arguments: []))
+      return osc.incomingMessagesSignal
+        .filter { $0.address == "/live/name/scene" }
+        .observe { NSLog("  Got scene #\($0.arguments[0]): \($0.arguments[1])") }
+    }*/
   }
 }
 
