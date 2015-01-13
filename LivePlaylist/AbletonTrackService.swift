336645977dd22f0317cc3810af92a387c1370d34
Jari Aarniala <jari.aarniala@gmail.com>
2015-01-13 09:27:42 +0200

    Fix deadlock with delay(0). Still not working completely

--- LivePlaylist/AbletonTrackService.swift
+++ LivePlaylist/AbletonTrackService.swift
@@ -15,8 +15,8 @@ class AbletonTrackService : NSObject, TrackService {
         .take(1)
         .map { $0.arguments[0] as Int }
         // Recursive signal deadlocks, so delay the value (https://github.com/ReactiveCocoa/ReactiveCocoa/issues/1670)
-       
-
+        .delay(0, onScheduler: QueueScheduler())
+      
     let sceneNames : HotSignal<String> = numberOfScenes.mergeMap { n in
       NSLog("Will map \(n) tracks to their names")
 
@@ -27,19 +27,14 @@ class AbletonTrackService : NSObject, TrackService {
           .map { $0.arguments[1] as String }
 
       self.osc.sendMessage(OSCMessage(address: "/live/name/scene", arguments: []))
+      
+      sceneNameReplies.observe { NSLog("Inner got name \($0)") }
 
       return sceneNameReplies
     }
     
+    // TODO no replies received here!
     sceneNames.observe { NSLog("Got name \($0)") }
-    
-    /*let x : HotSignal<OSCMessage> = numberOfScenes.mergeMap { n in
-      NSLog("BOOM! Getting names for \(n) scenes")
-      osc.sendMessage(OSCMessage(address: "/live/name/scene", arguments: []))
-      return osc.incomingMessagesSignal
-        .filter { $0.address == "/live/name/scene" }
-        .observe { NSLog("  Got scene #\($0.arguments[0]): \($0.arguments[1])") }
-    }*/
   }
 }
 
