5ff1ad3e9feabac925a01d6bbd4ca28e42fe0b17
Jari Aarniala <jari.aarniala@gmail.com>
2015-01-21 19:29:25 +0200

    Add PlaylistView inside our new main controller

--- LivePlaylist/MainViewController.swift
+++ LivePlaylist/MainViewController.swift
@@ -3,6 +3,10 @@ import UIKit
 class MainViewController: UIViewController {
   override init() {
     super.init(nibName: nil, bundle: nil);
+
+    let playlistViewController = PlaylistViewController()
+    addChildViewController(playlistViewController)
+    view!.addSubview(playlistViewController.view)
   }
   
   required init(coder: NSCoder) {
