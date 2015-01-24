5dd984a5d07bc6309ce54fe80dba8d2cabdebfa5
Jari Aarniala <jari.aarniala@gmail.com>
2015-01-24 15:12:44 +0200

    Add text field for IP address; WIP

--- LivePlaylist/SettingsViewController.swift
+++ LivePlaylist/SettingsViewController.swift
@@ -16,16 +16,23 @@ class SettingsViewController: UIViewController {
   override func viewDidLoad() {
     super.viewDidLoad()
     
-    let settingsButton = UIButton()
-    settingsButton.setTitle("Nappi", forState: .Normal)
-    settingsButton.titleLabel?.font = UIFont(name: "Avenir", size: 25)
-    settingsButton.backgroundColor = UIColor.grayColor()
-    settingsButton.layer.cornerRadius = 3
-    settingsButton.addTarget(self, action: "dismiss", forControlEvents: .TouchUpInside)
-    view!.addSubview(settingsButton)
+    let title = UILabel()
+    title.text = "Server address"
+    view.addSubview(title)
+    
+    let ipAddressTextField = UITextField()
+    ipAddressTextField.text = "192.168.0.1"
+    view.addSubview(ipAddressTextField)
+    
+    let helpText = UILabel()
+    helpText.text = "Enter the IP address of the computer that's running Ableton Live."
+    view.addSubview(helpText)
 
-    layout(settingsButton) { settings in
-      settings.size == settings.superview!.size / 2; return
+    layout(title, ipAddressTextField, helpText) { title, ipAddressTextField, helpText in
+      // TODO hackish way to set top margin
+      title.top == title.superview!.top + 40
+      ipAddressTextField.top == title.bottom + 10
+      helpText.top == ipAddressTextField.bottom + 10
     }
   }
   
