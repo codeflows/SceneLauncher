769961f77723f8f68c5c6ca809cf8e445847357e
Jari Aarniala <jari.aarniala@gmail.com>
2015-01-24 15:25:55 +0200

    Add fonts, tune margins

--- LivePlaylist/SettingsViewController.swift
+++ LivePlaylist/SettingsViewController.swift
@@ -18,21 +18,33 @@ class SettingsViewController: UIViewController {
     
     let title = UILabel()
     title.text = "Server address"
+    title.font = UIFont(name: "Avenir", size: 25)
     view.addSubview(title)
     
     let ipAddressTextField = UITextField()
     ipAddressTextField.text = "192.168.0.1"
+    ipAddressTextField.font = UIFont(name: "Avenir", size: 20)
     view.addSubview(ipAddressTextField)
     
     let helpText = UILabel()
     helpText.text = "Enter the IP address of the computer that's running Ableton Live."
+    helpText.font = UIFont(name: "Avenir", size: 18)
+    helpText.numberOfLines = 0
     view.addSubview(helpText)
 
     layout(title, ipAddressTextField, helpText) { title, ipAddressTextField, helpText in
       // TODO hackish way to set top margin
       title.top == title.superview!.top + 40
+      title.left == title.superview!.left + 10
+      title.width == title.superview!.width - 20
+      
       ipAddressTextField.top == title.bottom + 10
+      ipAddressTextField.left == title.left
+      ipAddressTextField.width == title.width
+
       helpText.top == ipAddressTextField.bottom + 10
+      helpText.left == title.left
+      helpText.width == title.width
     }
   }
   
