cf0e4b01bf27d95b5d287d40da58a3d449acbd03
Jari Aarniala <jari.aarniala@gmail.com>
2015-01-25 14:26:49 +0200

    Use different background color

--- src/PlaylistCell.swift
+++ src/PlaylistCell.swift
@@ -6,11 +6,12 @@ class PlaylistCell: UICollectionViewCell {
   override func layoutSubviews() {
     titleLabel.frame = self.contentView.bounds
     titleLabel.font = UIFont(name: "Avenir", size: 18)
+    titleLabel.textColor = UIColor.whiteColor()
   }
   
   override init(frame: CGRect) {
     super.init(frame: frame)
-    contentView.backgroundColor = UIColor.greenColor()
+    contentView.backgroundColor = UIColor(red: 169/255.0, green: 31/255.0, blue: 199/255.0, alpha: 1.0)
     self.contentView.addSubview(titleLabel)
   }
   
