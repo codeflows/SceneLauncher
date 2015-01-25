cb4502f5633d4dd8f6fdfb44269c53d772b730f1
Jari Aarniala <jari.aarniala@gmail.com>
2015-01-25 15:02:27 +0200

    Highlight button when pressed

--- src/PlaylistCell.swift
+++ src/PlaylistCell.swift
@@ -2,11 +2,25 @@ import UIKit
 
 class PlaylistCell: UICollectionViewCell {
   let titleLabel = UILabel()
+  let purpleColor = UIColor(red: 169/255.0, green: 31/255.0, blue: 199/255.0, alpha: 1.0)
+  
+  // TODO jari: cleanup
+  override var highlighted: Bool {
+    get {
+      return super.highlighted
+    }
+    set(h) {
+      super.highlighted = h
+      titleLabel.backgroundColor = h ?
+        purpleColor.colorWithAlphaComponent(0.6) :
+        purpleColor
+    }
+  }
   
   override func layoutSubviews() {
     titleLabel.frame = self.contentView.bounds
     titleLabel.font = UIFont(name: "Avenir", size: 18)
-    titleLabel.backgroundColor = UIColor(red: 169/255.0, green: 31/255.0, blue: 199/255.0, alpha: 1.0)
+    titleLabel.backgroundColor = purpleColor
     titleLabel.textColor = UIColor.whiteColor()
   }
   
