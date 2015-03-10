014fb8052e21e5263fed066581f349e5643740e2
Jari Aarniala <jari.aarniala@gmail.com>
2015-03-10 22:48:23 +0200

    Also timeout scene name response

--- src/AbletonTrackService.swift
+++ src/AbletonTrackService.swift
@@ -8,10 +8,10 @@ class AbletonTrackService : NSObject, TrackService {
   }
 
   func listTracks(callback: ([Track]?) -> ()) {
-    // TODO timeout handling for all the requests
     // TODO currently many requests might be waiting at the same time
-    // TODO LiveOsc(?) fails if Scene name contains Unicode characters and returns /remix/error
+    // TODO LiveOsc(?) fails if Scene name contains Unicode characters and returns /remix/error -> short-circuit signal here
     // TODO reliable mechanism for pinging if the server is still reachable
+    // TODO refactor: abstraction for sending and receiving message of the same address (e.g. both cases below)
 
     let numberOfScenes : Signal<Int, NSError> =
       osc.incomingMessagesSignal
@@ -20,35 +20,41 @@ class AbletonTrackService : NSObject, TrackService {
         |> map { $0.arguments[0] as Int }
         |> timeoutWithError(NSError(), afterInterval: 5, onScheduler: QueueScheduler.mainQueueScheduler)
 
-    let message = OSCMessage(address: "/live/scenes", arguments: [])
-    osc.sendMessage(message)
+    osc.sendMessage(OSCMessage(address: "/live/scenes", arguments: []))
     
-    // TODO really, we'd like to flatMap the Signal(numberOfScenes) to Signal([Track]) and return that
+    // TODO really, we'd like to flatMap the Signal(numberOfScenes) to Signal([Track]) and timeout in one place
     numberOfScenes.observe(next: { expectedNumberOfScenes in
-      let scenesSignal : Signal<[Track], NSError> =
-        self.osc.incomingMessagesSignal
-          |> filter { $0.address == "/live/name/scene" }
-          |> take(expectedNumberOfScenes)
-          |> map { Track(order: $0.arguments[0] as Int, name: $0.arguments[1] as String) }
-          |> scan([], { $0 + [$1] })
-          // FIXME ugly: scan returns intermittent results, only choose the last one (use ColdSignal?)
-          |> filter { $0.count == expectedNumberOfScenes }
-          |> take(1)
-          
-      let sortedScenesSignal = scenesSignal |> map { scenes in
-        scenes.sorted({$0.order < $1.order})
-      }
-
+      self.handleTrackListResponse(callback, expectedNumberOfScenes: expectedNumberOfScenes)
       self.osc.sendMessage(OSCMessage(address: "/live/name/scene", arguments: []))
-      
-      // TODO handle UI thread stuff in the view controller
-      let tempSignal = sortedScenesSignal |> observeOn(UIScheduler())
-      tempSignal.observe(callback)
     }, error: { err in
       // TODO
-      println("Error received")
+      println("Timeout in number of scenes response")
       callback(nil)
     })
+  }
+  
+  private func handleTrackListResponse(callback: ([Track]?) -> (), expectedNumberOfScenes: Int) {
+    let scenesSignal : Signal<[Track], NSError> =
+      osc.incomingMessagesSignal
+        |> filter { $0.address == "/live/name/scene" }
+        |> take(expectedNumberOfScenes)
+        |> map { Track(order: $0.arguments[0] as Int, name: $0.arguments[1] as String) }
+        |> scan([], { $0 + [$1] })
+        // FIXME ugly: scan returns intermittent results, only choose the last one (use SignalProducer.takeLast?)
+        |> filter { $0.count == expectedNumberOfScenes }
+        |> take(1)
+        |> timeoutWithError(NSError(), afterInterval: 5, onScheduler: QueueScheduler.mainQueueScheduler)
+    
+    let sortedScenesSignal = scenesSignal |> map { scenes in
+      scenes.sorted({$0.order < $1.order})
+    }
+    
+    // TODO handle UI thread stuff in the view controller
+    let tempSignal = sortedScenesSignal |> observeOn(UIScheduler())
+    tempSignal.observe(
+      next: callback,
+      error: { err in println("Timeout in track list response"); callback(nil) }
+    )
   }
 }
 
