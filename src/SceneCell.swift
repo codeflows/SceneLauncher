d1c15ec0251b885e38da482ca216ee82cccf8e07
Jari Aarniala <jari.aarniala@gmail.com>
2015-04-01 01:11:28 +0300

    Draw bottom border on scene

--- src/SceneCell.swift
+++ src/SceneCell.swift
@@ -9,10 +9,15 @@ class SceneCell: UICollectionViewCell {
     super.init(frame: frame)
     self.contentView.addSubview(titleLabel)
 
-    self.contentView.backgroundColor = purpleColor
+    self.contentView.backgroundColor = purpleColor.colorWithAlphaComponent(0.1)
+
+    var bottomBorder = CALayer()
+    bottomBorder.backgroundColor = purpleColor.colorWithAlphaComponent(0.7).CGColor
+    bottomBorder.frame = CGRect(x: 0, y: self.contentView.frame.height - 0.5, width: self.contentView.frame.width, height: 0.5)
+    self.contentView.layer.addSublayer(bottomBorder)
     
     titleLabel.font = UIFont(name: UIConstants.fontName, size: 18)
-    titleLabel.textColor = UIColor.whiteColor()
+    titleLabel.textColor = UIColor.blackColor()
 
     layout(titleLabel) { titleLabel in
       titleLabel.height == titleLabel.superview!.height
