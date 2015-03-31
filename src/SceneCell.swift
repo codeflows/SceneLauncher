72f7ef37098966c953d4abcf86c46ebe0c98c439
Jari Aarniala <jari.aarniala@gmail.com>
2015-04-01 01:06:41 +0300

    Autolayout scene cell

--- src/SceneCell.swift
+++ src/SceneCell.swift
@@ -1,19 +1,24 @@
 import UIKit
+import Cartography
 
 class SceneCell: UICollectionViewCell {
   let titleLabel = UILabel()
   let purpleColor = UIColor(red: 169/255.0, green: 31/255.0, blue: 199/255.0, alpha: 1.0)
 
-  override func layoutSubviews() {
-    titleLabel.frame = self.contentView.bounds
-    titleLabel.font = UIFont(name: UIConstants.fontName, size: 18)
-    titleLabel.backgroundColor = purpleColor
-    titleLabel.textColor = UIColor.whiteColor()
-  }
-  
   override init(frame: CGRect) {
     super.init(frame: frame)
     self.contentView.addSubview(titleLabel)
+
+    self.contentView.backgroundColor = purpleColor
+    
+    titleLabel.font = UIFont(name: UIConstants.fontName, size: 18)
+    titleLabel.textColor = UIColor.whiteColor()
+
+    layout(titleLabel) { titleLabel in
+      titleLabel.height == titleLabel.superview!.height
+      titleLabel.left == titleLabel.superview!.left + UIConstants.margin
+      titleLabel.right == titleLabel.superview!.right - UIConstants.margin
+    }
   }
   
   required init(coder aDecoder: NSCoder) {
