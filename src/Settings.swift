a13e8efe3c64f643f557eb56fc576bc098a0eb77
Jari Aarniala <jari.aarniala@gmail.com>
2015-04-06 11:57:10 +0300

    Skip initial value

--- src/Settings.swift
+++ src/Settings.swift
@@ -5,7 +5,12 @@ struct Settings {
   private static let serverAddressKey = "SceneLauncher.serverAddress"
   
   // TODO only let non-empty-values out?
-  static let serverAddress = MutableProperty(NSUserDefaults.standardUserDefaults().stringForKey(serverAddressKey))
+  static let serverAddress = { () -> MutableProperty<String?> in
+    let property = MutableProperty(NSUserDefaults.standardUserDefaults().stringForKey(serverAddressKey))
+    let ignoreInitialValue: SignalProducer<String?, NoError> = property.producer |> skip(1)
+    ignoreInitialValue.start(next: { println("Yes sir got \($0)") })
+    return property
+  }()
 
   // TODO remove
   static func getServerAddress() -> String? {
