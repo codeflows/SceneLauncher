ba079c99c92a97d41cc8b647c975adb7e76b9e2f
Jari Aarniala <jari.aarniala@gmail.com>
2015-04-06 12:01:17 +0300

    Save server address when it changes

--- src/Settings.swift
+++ src/Settings.swift
@@ -8,7 +8,10 @@ struct Settings {
   static let serverAddress = { () -> MutableProperty<String?> in
     let property = MutableProperty(NSUserDefaults.standardUserDefaults().stringForKey(serverAddressKey))
     let ignoreInitialValue: SignalProducer<String?, NoError> = property.producer |> skip(1)
-    ignoreInitialValue.start(next: { println("Yes sir got \($0)") })
+    // TODO compiler crashes if we do "producer |> skip |> start"
+    ignoreInitialValue.start(next: {
+      NSUserDefaults.standardUserDefaults().setObject($0, forKey: serverAddressKey)
+    })
     return property
   }()
 
@@ -19,7 +22,5 @@ struct Settings {
   
   static func setServerAddress(newAddress: String) {
     serverAddress.put(newAddress)
-    // TODO do this when serverAddress changes
-    NSUserDefaults.standardUserDefaults().setObject(newAddress, forKey: serverAddressKey)
   }
 }
